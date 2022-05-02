2a17e081066c36ee2af75fa4b80c3c8fbfdb9a8a
AkashdeepArjun <wadhwaarjun007@gmail.com>
2022-05-02 04:05:06 -0700

    main class to test decorator pattern

---
+++ src/home/Test.java
@@ -0,0 +1,13 @@
+package home;
+
+public class Test {
+    
+
+    public static void main(String[] args){
+
+        Android android = new SmartWatch(new SmartPhone());
+        android.smartFeatures();
+
+    }
+    
+}
