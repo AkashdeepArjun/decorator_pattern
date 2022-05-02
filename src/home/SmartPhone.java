41b32b92dd461ead979e60c42518cdd4a11fc3a8
AkashdeepArjun <wadhwaarjun007@gmail.com>
2022-05-02 04:02:57 -0700

    class that implements android interface

---
+++ src/home/SmartPhone.java
@@ -0,0 +1,12 @@
+package home;
+
+public class SmartPhone implements Android{
+    @Override
+    public void smartFeatures() {
+        MyUtils.log("SMARTPHONE", "can play music");
+        MyUtils.log("SMARTPHONE", "can play vidoes");
+        MyUtils.log("SMARTPHONE", "can show weather");
+        MyUtils.log("SMARTPHONE", "can browse internet");
+        
+    }
+}
